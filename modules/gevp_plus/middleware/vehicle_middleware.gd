0d59c8ff45db31d551495c5b74801e3ee1b73ac6
GekkotaDev <114091010+GekkotaDev@users.noreply.github.com>
2025-10-07 00:09:54 +0800

    feat: vehicle state middleware system

---
+++ modules/gevp_plus/middleware/vehicle_middleware.gd
@@ -0,0 +1,9 @@
+class_name VehicleMiddleware extends Node
+
+
+func integrate_forces(_state: PhysicsDirectBodyState3D):
+    pass
+
+
+func process_vehicle(_vehicle: Vehicle, _camera: Camera3D, _delta: float):
+    pass
---
+++ modules/gevp_plus/middleware/vehicle_state.gd
@@ -0,0 +1,7 @@
+class_name VehicleState extends Resource
+
+@export_group("Camera")
+@export var fov: int
+
+@export_group("Vehicle")
+@export var torque: float
