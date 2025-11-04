88bab99660b6dfa13ca7775a4aad254466e084a2
GekkotaDev <114091010+GekkotaDev@users.noreply.github.com>
2025-11-04 22:20:10 +0800

    feat: basic dialog implementation

--- modules/dialog/DialogController.cs
+++ modules/dialog/DialogController.cs
@@ -1,10 +1,18 @@
 using DialogueManagerRuntime;
+using DotNext.Collections.Generic;
 using Godot;
 using System;
 
 public partial class DialogController : Node {
-    async public void QueueDialogue(Resource line, String id) {
-        var nextLine = await DialogueManager.GetNextDialogueLine(line, "start");
-        QueueDialogue(line, nextLine.NextId);
+    async public void QueueDialogue(
+            Resource dialog,
+            string id,
+            Action<DialogueLine>[] visitors
+        ) {
+        var response = await DialogueManager.GetNextDialogueLine(dialog, id);
+        if (response?.NativeInstance == null) return;
+
+        foreach (var callback in visitors) callback(response);
+        QueueDialogue(dialog, response.NextId, visitors);
     }
 }
